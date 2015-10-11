b3c7856e9bf8652c5e4aae12041ca7f03f275895
ティン・ルーフ <tinruufu@gmail.com>
2015-10-11 20:46:28 +0100

    lather rinse repeat

--- tweet.py
+++ tweet.py
@@ -3,6 +3,9 @@ import tweepy
 from miserable import generate
 from secrets import app_key, app_secret, token_key, token_secret
 
+status = generate()
+print status
+
 auth = tweepy.OAuthHandler(app_key, app_secret)
 auth.set_access_token(token_key, token_secret)
-tweepy.API(auth).update_status(status=generate())
+tweepy.API(auth).update_status(status=status)
