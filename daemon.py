14d6fa8d71b1c9cd3cfc5f01b504a7af30f1616c
ティン・ルーフ <tinruufu@gmail.com>
2015-10-12 09:18:58 +0100

    twoot more

--- daemon.py
+++ daemon.py
@@ -2,8 +2,8 @@ from subprocess import check_call
 from time import sleep
 from random import random
 
-MIN_PAUSE = 3 * 60 * 60
-MAX_PAUSE = 9 * 60 * 60
+MIN_PAUSE = 2 * 60 * 60
+MAX_PAUSE = 6 * 60 * 60
 DELTA = MAX_PAUSE - MIN_PAUSE
 
 # i dont want to store the entire wordnet corpus in memory at all times on my
